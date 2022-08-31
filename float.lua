3b10db43b050d96c7dc359157f2908306ce0d552
Sathvik <88944590+User319183@users.noreply.github.com>
2022-08-31 17:14:35 -0400

    Update float.lua

--- float.lua
+++ float.lua
@@ -6,6 +6,5 @@ player.Character.HumanoidRootPart.Size = Vector3.new(10, 10, 10)
 
 
 player.Character.HumanoidRootPart.CanCollide = false
-player.Character.HumanoidRootPart.CanCollide = true
 player.Character.HumanoidRootPart.CanCollide = false
-    
+ 
