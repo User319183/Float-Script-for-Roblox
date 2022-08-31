2ce36b329134e84b3ac2de94b078a9da0d417f52
Sathvik <88944590+User319183@users.noreply.github.com>
2022-08-31 17:11:11 -0400

    Create float.lua

---
+++ float.lua
@@ -0,0 +1,11 @@
+local player = game.Players.LocalPlayer
+
+
+player.Character.HumanoidRootPart.Size = Vector3.new(10, 10, 10)
+
+
+
+player.Character.HumanoidRootPart.CanCollide = false
+player.Character.HumanoidRootPart.CanCollide = true
+player.Character.HumanoidRootPart.CanCollide = false
+    
